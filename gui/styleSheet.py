8d140cd6121fb41a1b0a18d21dcbaa7536ec98ab
Alok Gandhi <alok.gandhi2002@gmail.com>
2013-04-01 16:34:11 -0400

    simplified stylesheet code, deleted unnecessary stylesheets

--- gui/styleSheet.py
+++ gui/styleSheet.py
@@ -24,59 +24,14 @@
 import sys
 import os
 from PyQt4 import QtCore, QtGui
-from logger import Logger
-
-
-APP_STYLE = ("WindowsVista" if sys.platform.startswith('win')  else "Plastique")
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 class StyleSheet(object):
-    STYLESHEET_OPTIONS = ['dark', 'soft', 'maya', 'nuke',]
-
     def __init__(self, *args, **kwargs):
         super(StyleSheet, self).__init__(*args, **kwargs)
-        self.prefFile = os.path.join(ROOT_DIR, 'prefs', 'currStyle')
-        self.style = ''
 
-    def _createPrefs(self):
-        with open(self.prefFile, 'w') as f:
-            f.write('theme:dark')
-
-    def _readPrefs(self):
-        if not os.path.exists(self.prefFile):
-            self._createPrefs()
-
-        with open(self.prefFile, 'r') as f:
-            s = f.readlines()
-
-        self.style= s[0].split(':')[1]
-
-        return self.style
-
-    def _writePrefs(self, pref=''):
-        if not os.path.exists(self.prefFile):
-            self._createPrefs()
-
-        with open(self.prefFile, 'w') as f:
-            f.write('theme:%s:' % pref)
-
-    def setColor(self, widget, app=None, init=False):
-        self._readPrefs()
-
-        if not self.style:
-            if app:
-                pass
-                app.setStyle(QtGui.QStyleFactory.create(APP_STYLE))
-
-            widget.setStyleSheet("")
-
-            return
-
-        if self.style not in self.STYLESHEET_OPTIONS:
-            raise Exception('"%s" type of stylesheet option does not exist !!'
-                            % self.style)
-
-        p = os.path.join(ROOT_DIR, 'styleSheets', str(self.style))
+    def setColor(self, widget, app=None):
+        p = os.path.abspath(os.path.join(ROOT_DIR, 'styleSheets', 'stylesheet'))
 
         if not os.path.exists(p):
             raise Exception('Style Path - %s does not exist !!' % p)
@@ -84,9 +39,6 @@ class StyleSheet(object):
         s = ''
         with open(p, 'r') as f:
             s = f.read()
-        #
-        #imageDir = str(os.path.abspath(os.path.join(ROOT_DIR, 'icons')))
-        #s = s.replace('FULLPATH', imageDir)
 
         if app:
             app.setStyle(QtGui.QStyleFactory.create("Plastique"))
